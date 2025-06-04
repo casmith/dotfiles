da2c7622160fee507597ef520fd0bbda155177f0
Clay Smith <casmith@users.noreply.github.com>
2025-06-03 19:39:39 -0500

    fix issues with install script

--- .github/install.sh
+++ .github/install.sh
@@ -1,7 +1,11 @@
 mkdir -p tmp/old
-mv .bashrc .bash_profile other things tmp/old
-git init
-git remote add origin git@github.com:casmith/dotfiles.git 
+mv .bashrc .bash_profile other things .zshrc tmp/old 2> /dev/null
+
+git init --initial-branch main
+git remote add origin https://github.com/casmith/dotfiles.git 
 git pull origin main --ff-only
+git checkout main
 git branch --set-upstream-to origin/main main
+
+mkdir -p "$HOME/.ssh"
 chmod 0600 "$HOME/.ssh/config"
